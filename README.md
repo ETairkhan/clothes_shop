68245a7027817b022d1f4aea0a6322194e65b2da
ETairkhan <trouzerop@gmail.com>
2025-11-04 22:21:10 +0500

    did first tasks from the video

---
+++ README.md
@@ -0,0 +1,3 @@
+in package learningPython => there wrote the tasks to understand the python
+
+and the project overall for clothing here 
---
+++ learningPython/1Task.py
@@ -0,0 +1,20 @@
+appartment = int(input("Write the number of the appartment"))
+
+floar = 1 
+pod = 1
+temp = appartment
+if appartment <= 0:
+   print("norm appartment napishi")
+while(appartment > 0):
+   if appartment <= 20:
+      while(appartment > 0):
+         appartment = appartment - 4
+         if appartment <= 0:
+            print("этаж: " + str(floar))
+            break
+         floar += 1
+      print("подъезд: " , pod)
+      break
+   appartment = appartment - 20
+   pod += 1   
+
