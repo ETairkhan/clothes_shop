70bf0809d9c26f328b9edafeab30c23c2caebed1
ETairkhan <trouzerop@gmail.com>
2025-11-12 10:39:13 +0500

    course for beginners and learning python

---
+++ learningPython/3Task.py
@@ -0,0 +1,30 @@
+# my_string = "Hello, world"
+# print(my_string)
+
+# my_string = """
+# PYfjaskfj;asjf;lj
+# aksjf;asjf;ksajf;kjsa
+# askfjas;fj;sakfj;ljfsjf
+# """
+
+# print(my_string)
+
+# first_name = "Alice"
+# last_name = "Smith"
+
+# full_name = first_name + " " + last_name
+
+# print(full_name)
+
+# length = len(full_name)
+# print(length)
+
+
+# my_int = int("10")
+# print(type(my_int))
+# my_integer = 100 
+# my_string = str(my_integer)
+
+big_integer = 2 ** 1000 
+length = len(str(big_integer))
+print(length)
